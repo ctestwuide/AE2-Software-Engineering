b86f70e14674aac0b55f36d7fd52dfce638262bb
ctestwuide <117029690+ctestwuide@users.noreply.github.com>
2023-03-08 11:30:21 +0000

    changed import process so it asks users for files instead of auto

--- SE_AE2/AE2/DataImportExport.java
+++ SE_AE2/AE2/DataImportExport.java
@@ -11,14 +11,14 @@ public class DataImportExport {
 		System.out.println("Starting program..."
 				+ "\nThe program needs file paths to read and write data for different parts of the program."
 				+ "\nPlease specify 3 file paths for teaching requirements, part time teacher, and training sessions.");
-		
+		/*
 		filePathTR = "C:\\users\\chase\\desktop\\testTR.csv";
 		FileHandler.readTeachingRequirementFile(filePathTR);
 		filePathPTT = "C:\\users\\chase\\desktop\\testPTT.csv";
 		FileHandler.readPartTimeTeacherFile(filePathPTT);
 		filePathTS = "C:\\users\\chase\\desktop\\testTS.csv";
 		FileHandler.readPTTTrainingSessionFile(filePathTS);
-		/*
+		*/
 		System.out.print("\n   1-Enter file path for Teaching Requirement data: ");
 		filePathTR = getFilePath();
 		FileHandler.readTeachingRequirementFile(filePathTR);
@@ -30,7 +30,7 @@ public class DataImportExport {
 		System.out.print("\n   3-Enter file path for Training Session data: ");
 		filePathTS = getFilePath();
 		FileHandler.readPTTTrainingSessionFile(filePathTS);
-		*/
+		
 	}
 	
 	public static String getFilePath() {
